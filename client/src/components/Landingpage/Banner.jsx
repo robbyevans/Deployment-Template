c87c12ed237b333e3ca9f980684d6bfb6899f029
Evans Rutto <robbyevans001@gmail.com>
2022-12-24 15:20:01 +0300

    gone for holiday

--- client/src/components/Landingpage/Banner.jsx
+++ client/src/components/Landingpage/Banner.jsx
@@ -34,12 +34,24 @@
 
 
 import React from 'react'
+import "./Banner.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 function Banner() {
   return (
-    <div>Banner</div>
+    <div className='banner'>
+      <div className='banner-header'>
+        <div className='banner-title'>
+          <div className="main-title">
+            <h1>Get a pickup <span>Fast,Easy & Affordable</span></h1>
+          </div>
+        </div>
+        <div className='banner-image'>
+          <img src='./pickup02.jpg'></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
